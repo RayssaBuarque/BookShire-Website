e37371fa0c87713687cb7bc8cf13a52123217c5f
rayssabuarque <brayssa17@gmail.com>
2023-07-16 17:45:48 -0300

    galeria criada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { CardConquistaComponent } from './pages/home/card-conquista/card-conquista.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    CardConquistaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/home/card-conquista/card-conquista.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-conquista',
+  templateUrl: './card-conquista.component.html',
+  styleUrls: ['./card-conquista.component.css']
+})
+export class CardConquistaComponent implements OnInit {
+
+  //PROPRIEDADES INPUTÁVEIS DO CARD
+  @Input() id:string = '#'
+  @Input() title:string = 'Título do projeto'
+  @Input() desc:string = 'Descrição do projeto'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
