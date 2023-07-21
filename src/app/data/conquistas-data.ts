b1d6471a4d242c7039a34cc04fd023e6a04883e0
rayssabuarque <brayssa17@gmail.com>
2023-07-20 21:48:44 -0300

    projects data

---
+++ src/app/data/conquistas-data.ts
@@ -0,0 +1,85 @@
+// {
+    //     "id": "0",
+    //     "ano": "XXXX",
+    //     "titulo-rojeto":"desafio X",
+    //     "relevancia":"finalista"
+    //     "descricao-projeto":"descricao do projeto",
+    //     "pic": {
+    //         "picAltText": "descricao da imagem",
+    //         "picUrl":"../../assets/fotos/"
+    //     },
+    //     "links": [
+    //         {
+    //             "titulo-link": "Exemplo: vídeo-pitch",
+    //             "url-link": "url"
+    //         },
+    //         {
+    //             "titulo-link": "Exemplo: vídeo-docs",
+    //             "url-link": "url"
+    //         }
+    //     ]
+    // }
+
+export const devData = [
+    {
+        "id": "1",
+        "ano": "2022",
+        "titulo-rojeto":"Hackathon Etec Abdias",
+        "relevancia":"Primeira Aparição do Projeto",
+        "descricao-projeto":"O BookShire foi inicialmente desenvolvido como um projeto para o 5º Hackathon Acadêmico do Centro Paula Souza em Agosto de 2022. Apesar de não ter chegado nas finais, a ideia continou sendo trabalhada pela equipe após a apresentação do Pitch presencial no auditório da Etec Abdias do Nascimento.",
+        "pic": {
+            "picAltText": "Dois jovens com um microfone apresentando a aplicação BookShire em um palco de auditório.",
+            "picUrl":"../../assets/fotos/pitch_Hackathon2022.jpg"
+        },
+        "links": [
+            {
+                "titulo-link": "Primeiro Vídeo Pitch do BookShire",
+                "url-link": "https://youtu.be/SeB-qOkPGhs"
+            }
+        ]
+    },
+
+    {
+        "id": "2",
+        "ano": "2022 - 2023",
+        "titulo-rojeto":"Desafio Liga Jovem",
+        "relevancia":"Semi Finalistas",
+        "descricao-projeto":"Alguns meses após nossa participação no Hackathon, o BookShire recebeu uma repaginada e diversas melhorias ao ser inscrito na primeira edição do Desafio Liga Jovem no Brasil. O BookShire foi selecionado como semi-finalista do programa dentre os 600 projetos participantes e nossa equipe apresentou nosso pitch novamente, dessa vez na Faculdade Sebrae.",
+        "pic": {
+            "picAltText": "Duas jovens segurando um microfone, apresentando um slideshow contendo a logo do BookShire para um círculo de jurados.",
+            "picUrl":"../../assets/fotos/pitch_LigaJovem.jpg"
+        },
+        "links": [
+            {
+                "titulo-link": "Segundo Vídeo Pitch do BookShire",
+                "url-link": "https://youtu.be/WgAgBCC5m3Y"
+            },
+            {
+                "titulo-link": "Artigo sobre as equipes semi-finalistas da Etec Abdias do Nascimento",
+                "url-link": "https://www.cps.sp.gov.br/treze-equipes-de-oito-etecs-estao-na-semifinal-do-desafio-liga-jovem/"
+            }
+        ]
+    },
+    
+    {
+        "id": "3",
+        "ano": "2023",
+        "titulo-rojeto":"Technovation Girls",
+        "relevancia":"Projeto Finalista do Brasil",
+        "descricao-projeto":"O BookShire está atualmente colocado entre os 5 projetos de empreendedorismo de liderança feminina brasileiros semifinalistas da divisão Sênior da competição Technovation 2023.",
+        "pic": {
+            "picAltText": "Duas garotas em cima de um palco de auditório, a logo do BookShire paira sobre um telão ao fundo.",
+            "picUrl":"../../assets/fotos/pitch_TechnovationGirls2023.png"
+        },
+        "links": [
+            {
+                "titulo-link": "Vídeo Pitch de Submissão do projeto",
+                "url-link": "https://youtu.be/Fse78UTc_lY"
+            },
+            {
+                "titulo-link": "Vídeo Demo do BookShire",
+                "url-link": "https://youtu.be/yCBzsE2NPG0"
+            }
+        ]
+    }
+]
