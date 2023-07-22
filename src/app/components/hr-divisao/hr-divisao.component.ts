cb3546e07123515fc204157fa5eb5a91b8707831
rayssabuarque <brayssa17@gmail.com>
2023-07-21 21:17:03 -0300

    mild adjustments

--- src/app/components/hr-divisao/hr-divisao.component.ts
+++ src/app/components/hr-divisao/hr-divisao.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hr-divisao',
   templateUrl: './hr-divisao.component.html',
-  styleUrls: ['./hr-divisao.component.css']
+  styleUrls: [
+    './hr-divisao.component.css',
+    './hr-divisao.component-responsive.css'
+  ]
 })
 export class HrDivisaoComponent implements OnInit {
 
--- src/app/pages/home/header/header.component.ts
+++ src/app/pages/home/header/header.component.ts
@@ -21,6 +21,11 @@ export class HeaderComponent implements OnInit {
     this.onResize();
   }
 
+  openFigma(){
+    window.open('https://www.figma.com/proto/A857iR5L78vlbvgpo67oCM/Projeto_completo-_TCC?node-id=17-152&starting-point-node-id=17%3A152');
+    console.log('aaa')
+  }
+
   //checando se o prototipo precisa aparecer
   @HostListener('window:resize', ['$event']) onResize(event? : any) {
     if (window.innerWidth < 800){
