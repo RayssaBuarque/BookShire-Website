28d5c6d1913d9a2eae00890f8d00d91159c63630
rayssabuarque <brayssa17@gmail.com>
2023-07-23 19:08:28 -0300

    responsitivity

--- src/app/pages/home/card-info-pesquisa/card-info-pesquisa.component.ts
+++ src/app/pages/home/card-info-pesquisa/card-info-pesquisa.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-card-info-pesquisa',
   templateUrl: './card-info-pesquisa.component.html',
-  styleUrls: ['./card-info-pesquisa.component.css']
+  styleUrls: [
+                './card-info-pesquisa.component.css',
+                './card-info-pesquisa.component-responsive.css'
+              ]
 })
 export class CardInfoPesquisaComponent implements OnInit {
 
