13584b6369d9c313aa23ff1bdda236988cbb9e5c
rayssabuarque <brayssa17@gmail.com>
2023-12-11 16:58:45 -0300

    tornando botões responsivos

--- src/app/pages/galeria/galeria.component.ts
+++ src/app/pages/galeria/galeria.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GaleriaComponent implements OnInit {
 
+  index_conteudo:string = '1'
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showMonografia():void{
+    this.index_conteudo = '0'
+  }
+
+  showDesenvolvimento():void{
+    this.index_conteudo = '1'
+  }
+
+  showProjetos():void{
+    this.index_conteudo = '2'
   }
 
 }
